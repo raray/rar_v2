f567e13767ae4ef496406c08127529d3726c418f
R.A. Ray <rray@kalkomey.com>
2015-05-04 12:04:26 -0500

    some code cleanup

--- src/js/breakpoints.js
+++ src/js/breakpoints.js
@@ -1,14 +1,15 @@
+
 var RARay = RARay || {};
 
 RARay.breakpoints = {
 
-	// from http://tech.particulate.me/javascript/2013/10/10/how-to-conveniently-check-for-responsive-breakpoints-in-javascript/
-	get: function() {
-		
+  // from http://tech.particulate.me/javascript/2013/10/10/how-to-conveniently-check-for-responsive-breakpoints-in-javascript/
+  get: function() {
+
     var tag = window.getComputedStyle(document.body,':after');
         tag = tag.getPropertyValue('content');
         tag = tag.replace( /"/g,'');   // Firefox bugfix
-	   
+
     return tag;
-	}
+  }
 };
